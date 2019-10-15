e0195c1d4fb7c2854583a01f927bdb00276a2a21
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-10-16 00:25:56 +0530

    team name added as label

--- js/monitor.js
+++ js/monitor.js
@@ -1,80 +1,84 @@
 
 function getGithub() {
     let username = document.getElementById("username-box").value;
-    if(username == ""){
+    let teamname = document.getElementById("teamname-box").value;
+    if (username == "") {
         alert("Invalid Username");
+    } else if (teamname == "") {
+        alert("Invalid Team name");
     } else {
-    let urlbuilder = 'https://api.github.com/search/issues?q=created:>=2019-10-01+is:pr+is:public+author:' + username;
+        let urlbuilder = 'https://api.github.com/search/issues?q=created:>=2019-10-01+is:pr+is:public+author:' + username + "+label:\"" + teamname + "\"";
+        
+        $.ajax({
+            method: "GET",
+            url: urlbuilder,
+            dataType: "json",
+            cache: false,
+            success: function (response) {
+                console.log(response.total_count);
 
-    $.ajax({
-        method: "GET",
-        url: urlbuilder,
-        dataType: "json",
-        cache: false,
-        success: function (response) {
-            console.log(response.total_count);
-            //Welcome Text
-            document.getElementById("welcome-title").innerHTML = "Welcome " + response.items[0].user.login;
+                //Welcome Text
+                document.getElementById("welcome-title").innerHTML = "Welcome " + response.items[0].user.login;
 
-            //Profile Picture
-            document.getElementById("profile-pic").src = response.items[0].user.avatar_url;
+                //Profile Picture
+                document.getElementById("profile-pic").src = response.items[0].user.avatar_url;
 
-            // Contributions Count
-            document.getElementById("contributions").innerHTML = "Contributions: " + response.total_count;
+                // Contributions Count
+                document.getElementById("contributions").innerHTML = "Contributions for team " + teamname[0].toUpperCase() + teamname.slice(1).toLowerCase() + ": " + response.total_count;
 
-            //CleanUP Div
-            document.getElementById("progress-list").innerHTML = "";
+                //CleanUP Div
+                document.getElementById("progress-list").innerHTML = "";
 
-            //PRs
-            for (let i = 0; i <= response.items.length; i++) {
-                let element = document.getElementById("progress-list")
-                element.innerHTML = element.innerHTML +
-                    " <li>" +
-                    "<div>" +
-                    "<h4>" + response.items[i].repository_url.substr(29) + "</h4>" +
-                    "<h6>" + response.items[i].title + "</h6>" +
-                    "</div>" +
-                    "</li>"
-                    ;
+                //PRs
+                for (let i = 0; i <= response.items.length; i++) {
+                    let element = document.getElementById("progress-list")
+                    element.innerHTML = element.innerHTML +
+                        " <li>" +
+                        "<div>" +
+                        "<h4>" + response.items[i].repository_url.substr(29) + "</h4>" +
+                        "<h6>" + response.items[i].title + "</h6>" +
+                        "</div>" +
+                        "</li>"
+                        ;
+                }
             }
-        }
-    });
+        });
 
-    //Show the Div and scroll
-    document.getElementById("cont-div").style.display = "block";
-    window.scrollTo(0,500);
+        //Show the Div and scroll
+        document.getElementById("cont-div").style.display = "block";
+        window.scrollTo(0, 500);
 
 
-    // Http Request
-    // const Http = new XMLHttpRequest();
-    // const url = urlbuilder;
-    // Http.open("GET", url);
-    // Http.send();
-    // Http.onreadystatechange = (e) => {
-    //     let response = JSON.parse(Http.responseText);
-    //     //console.log(response.total_count)
+        // Http Request
+        // const Http = new XMLHttpRequest();
+        // const url = urlbuilder;
+        // Http.open("GET", url);
+        // Http.send();
+        // Http.onreadystatechange = (e) => {
+        //     let response = JSON.parse(Http.responseText);
+        //     //console.log(response.total_count)
 
-    //     //Welcome Text
-    //     document.getElementById("welcome-title").innerHTML = "Welcome " + response.items[0].user.login;
+        //     //Welcome Text
+        //     document.getElementById("welcome-title").innerHTML = "Welcome " + response.items[0].user.login;
 
-    //     //Profile Picture
-    //     document.getElementById("profile-pic").src = response.items[0].user.avatar_url;
+        //     //Profile Picture
+        //     document.getElementById("profile-pic").src = response.items[0].user.avatar_url;
 
-    //     // Contributions Count
-    //     document.getElementById("contributions").innerHTML = "Contributions: " + response.total_count;
+        //     // Contributions Count
+        //     document.getElementById("contributions").innerHTML = "Contributions: " + response.total_count;
 
-    //     //PRs
-    //     for (let i = 0; i <= response.items.length; i++) {
-    //         let element = document.getElementById("progress-list")
-    //         element.innerHTML = element.innerHTML +
-    //             " <li>" +
-    //             "<div>" +
-    //             "<h4>" + response.items[i].repository_url.substr(29) + "</h4>" +
-    //             "<h6>" + response.items[i].title + "</h6>" +
-    //             "</div>" +
-    //             "</li>"
-    //             ;
-    //     }
-    // }
+        //     //PRs
+        //     for (let i = 0; i <= response.items.length; i++) {
+        //         let element = document.getElementById("progress-list")
+        //         element.innerHTML = element.innerHTML +
+        //             " <li>" +
+        //             "<div>" +
+        //             "<h4>" + response.items[i].repository_url.substr(29) + "</h4>" +
+        //             "<h6>" + response.items[i].title + "</h6>" +
+        //             "</div>" +
+        //             "</li>"
+        //             ;
+        //     }
+        // }
     }
 }
