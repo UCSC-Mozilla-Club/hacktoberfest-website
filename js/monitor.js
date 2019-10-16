ba9af216f7d41db892eeadb502530b3fbcf1f50f
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-10-16 23:37:27 +0530

    team pr monitor

--- js/monitor.js
+++ js/monitor.js
@@ -7,7 +7,11 @@ function getGithub() {
     } else if (teamname == "") {
         alert("Invalid Team name");
     } else {
+
+        //TODO: set time bound
         let urlbuilder = 'https://api.github.com/search/issues?q=created:>=2019-10-01+is:pr+is:public+author:' + username + '+label:\"' + teamname + '\"';
+        
+        let teamUrlBuilder = 'https://api.github.com/search/issues?q=created:>=2019-10-01+is:pr+is:public+label:\"' + teamname + '\"';
         
         let userUrlBuilder = 'https://api.github.com/users/' + username;
 
@@ -39,14 +43,9 @@ function getGithub() {
             success: function (response) {
                 console.log(response.total_count);
 
-                //Welcome Text
-                // document.getElementById("welcome-title").innerHTML = "Welcome " + response.items[0].user.login;
-
-                //Profile Picture
-                // document.getElementById("profile-pic").src = response.items[0].user.avatar_url;
-
                 // Contributions Count
-                document.getElementById("contributions").innerHTML = "Contributions on team " + teamname[0].toUpperCase() + teamname.slice(1).toLowerCase() + ": " + response.total_count;
+                document.getElementById("contributions-user").innerHTML = "Your Contributions: " + response.total_count;
+                
 
                 //CleanUP Div
                 document.getElementById("progress-list").innerHTML = "";
@@ -58,6 +57,39 @@ function getGithub() {
                         " <li class='mb2'>" +
                         "<div>" +
                         "<h4>" + response.items[i].repository_url.substr(29) + "</h4>" +
+                        "<h6>" + response.items[i].title + "</h6>" +
+                        "</div>" +
+                        "</li>"
+                        ;
+                }
+            }
+        });
+
+        $.ajax({
+            method: "GET",
+            url: teamUrlBuilder,
+            dataType: "json",
+            cache: false,
+            success: function (response) {
+                console.log(response.total_count);
+
+                //team name
+                document.getElementById("contributions-team-name").innerHTML = "Team: " + teamname[0].toUpperCase() + teamname.slice(1).toLowerCase();
+
+                // Contributions Count
+                document.getElementById("contributions-team").innerHTML = "Team Contributions: " + response.total_count;
+                
+
+                //CleanUP Div
+                document.getElementById("progress-list-team").innerHTML = "";
+
+                //PRs
+                for (let i = 0; i < response.items.length; i++) {
+                    let element = document.getElementById("progress-list-team")
+                    element.innerHTML = element.innerHTML +
+                        " <li class='mb2'>" +
+                        "<div>" +
+                        "<h4>" + response.items[i].repository_url.substr(29) + "<span style=\"color: #5c5c5c\"> ("+ response.items[i].user.login +")<span></h4>" +
                         "<h6>" + response.items[i].title + "</h6>" +
                         "</div>" +
                         "</li>"
