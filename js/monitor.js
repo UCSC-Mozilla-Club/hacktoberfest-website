dd320d7fbafade7b891d244c1c535e3cc752f154
suvink <suvin@richmun.org>
2019-10-17 22:13:25 +0530

    Added pre hacktoberfest logo

--- js/monitor.js
+++ js/monitor.js
@@ -101,38 +101,5 @@ function getGithub() {
         //Show the Div and scroll
         document.getElementById("cont-div").style.display = "block";
         window.scrollTo(0, 500);
-
-
-        // Http Request
-        // const Http = new XMLHttpRequest();
-        // const url = urlbuilder;
-        // Http.open("GET", url);
-        // Http.send();
-        // Http.onreadystatechange = (e) => {
-        //     let response = JSON.parse(Http.responseText);
-        //     //console.log(response.total_count)
-
-        //     //Welcome Text
-        //     document.getElementById("welcome-title").innerHTML = "Welcome " + response.items[0].user.login;
-
-        //     //Profile Picture
-        //     document.getElementById("profile-pic").src = response.items[0].user.avatar_url;
-
-        //     // Contributions Count
-        //     document.getElementById("contributions").innerHTML = "Contributions: " + response.total_count;
-
-        //     //PRs
-        //     for (let i = 0; i <= response.items.length; i++) {
-        //         let element = document.getElementById("progress-list")
-        //         element.innerHTML = element.innerHTML +
-        //             " <li>" +
-        //             "<div>" +
-        //             "<h4>" + response.items[i].repository_url.substr(29) + "</h4>" +
-        //             "<h6>" + response.items[i].title + "</h6>" +
-        //             "</div>" +
-        //             "</li>"
-        //             ;
-        //     }
-        // }
     }
 }
